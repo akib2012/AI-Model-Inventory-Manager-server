b8f15486f2d7ffb128cf57f057f17d330220006f
akib bhuiyan <akibbhuiyan3544@gmail.com>
2025-11-08 22:32:30 +0600

    added env and more ferure

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+require('dotenv').config()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('ai inventor project server sunning ')
 })
 
-const uri = "mongodb+srv://ai_model_inventory_manager:vGwTWRaoUGn5UGHe@cluster0.exfto5h.mongodb.net/?appName=Cluster0";
+//  middle wire here
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.exfto5h.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,7 +30,21 @@ async function run() {
   try {
    
     await client.connect();
-    
+
+    // clreate collections here
+    const db = client.db('ai_model_inventory_manager');
+    const modelscollections = db.collection('models');
+
+
+    app.get('/models', async(req,res) => {
+        const cursor = modelscollections.find();
+        const  result  = await cursor.toArray();
+        res.send(result);
+    })
+
+
+
+
 
 
 
@@ -28,7 +52,7 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
@@ -40,7 +64,7 @@ run().catch(console.dir);
 
 
 // mongo user: ai_model_inventory_manager
-//  mongo pass: vGwTWRaoUGn5UGHe
+//  mongo pass: AAir8C8na2P1uNm1
 
 
 app.listen(port, () => {
